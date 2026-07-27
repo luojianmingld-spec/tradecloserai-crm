1d13b68237cdbdef32dbf06395fe2396d71169c4
root <root@vultr.guest>
2026-07-27 03:15:53 +0000

    fix: restore !isMobile on TG col-header to avoid duplicate title with mobile-header

--- frontend/src/views/LayoutView.vue
+++ frontend/src/views/LayoutView.vue
@@ -379,7 +379,7 @@
 
         <!-- ④ 聊天列表栏 -->
         <aside class="col-chatlist" :class="{ 'm-hidden': isMobile && mobileInConv && (activeConv || tgActiveJid) }">
-          <div class="col-header" v-if="activeChannel === 'telegram' && !tgActiveJid">
+          <div class="col-header" v-if="activeChannel === 'telegram' && !tgActiveJid && !isMobile">
             <div class="wa-col-brand">
               <svg viewBox="0 0 24 24" width="22" height="22" fill="#2AABEE"><path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.894 8.221l-1.97 9.28c-.145.658-.537.818-1.084.508l-3-2.21-1.446 1.394c-.14.18-.357.295-.6.295l.213-3.054 5.56-5.022c.24-.213-.054-.334-.373-.121l-6.869 4.326-2.96-.924c-.64-.203-.658-.64.135-.954l11.566-4.458c.538-.196 1.006.128.832.941z"/></svg>
               <span class="wa-col-title">Telegram</span>
